7a438ede0be4a1fd3e10c77a942cc25dfdc0befa
devrajmewada05 <devrajmewada840@gmail.com>
2026-01-28 23:33:56 +0530

    datatypeSolution

--- Lacture_12_Function/1_datatypes.cpp
+++ Lacture_12_Function/1_datatypes.cpp
@@ -4,16 +4,29 @@ using namespace std;
 void greet(){
     cout<<"hello"<<endl;
 }
-//argument and return
+//argument and no return
 void greet(string name){
     cout<<"hello"<<" "<<name<<endl;
 }
 //no argument and return
 int subtract(){
-    int a;
-    int b;
+    int a=4;
+    int b=3;
+    int c=a-b;
+    return c;
+    cout<<endl;
+}
+//argument and return
+int add(int a,int b){
+    int c=a+b;
+    cout<<endl;
+    return c;
 }
 int main()
 {
+    greet();
+    greet("dev");
+    cout<<subtract();
+    cout<<add(6,2);
    return 0;
 }
