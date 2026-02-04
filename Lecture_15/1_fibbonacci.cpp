4cc5536e9943fc7211de34b223544e64f9d8e798
devrajmewada05 <devrajmewada840@gmail.com>
2026-02-04 14:49:26 +0530

    notes

---
+++ Lecture_15/1_fibbonacci.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+   int fibbo(int n){
+//base
+if(n==0 || n==1){
+    return n;
+}
+return fibbo(n-1)+fibbo(n-2);
+}
+int main(){
+ int n;
+ cin>>n;
+ cout<<fibbo(n)<<endl;
+   return 0;
+}
