db7add82b187cb425dfaa08c23d6174f00ebd68c
devrajmewada05 <devrajmewada840@gmail.com>
2026-02-09 00:09:33 +0530

    notes

--- Practice/1que.cpp
+++ Practice/1que.cpp
@@ -1,3 +1,12 @@
+// 7
+//             1   
+//         2 1   1 2
+//     3 2 1       1 2 3
+// 4 3 2 1           1 2 3 4
+//     3 2 1       1 2 3
+//         2 1   1 2
+//             1
+
 #include<iostream>
 using namespace std;
 int main()
