2a231154ca64cca968f54f6b14e33dfcf77aa1be
devrajmewada05 <devrajmewada840@gmail.com>
2026-02-10 02:00:48 +0530

    notes

--- function_practice/11_pattern_inFunction.cpp
+++ function_practice/11_pattern_inFunction.cpp
@@ -1,3 +1,11 @@
+//       0
+//       4 0 4
+//     3 4 0 4 3
+//   2 3 4 0 4 3 2
+// 1 2 3 4 0 4 3 2 1
+
+//For right logic this code go on Practice folder......
+
 #include<iostream>
 using namespace std;
 void fun(int n){
@@ -10,18 +18,18 @@ void fun(int n){
                 cout<<"0 ";
             }
             else{
-                cout<<n-j<<" ";
+                cout<<n-j+1<<" ";
              }
         }
-        // for(int j=i;j>=1;j--){
-        //         cout<<n-j+1<<" ";
-        // }
+        for(int j=2;j<=i;j++){
+                cout<<n-j+1<<" ";
+        }
         cout<<endl;
     }
    
 }
 int main()
 {
-    fun(10);
+    fun(5);
    return 0;
 }
